620db82f072cc0e118e4ace7f5c6686debac756a
ayyxd <23397531+ayyxd@users.noreply.github.com>
2021-12-02 14:10:24 +0100

    Create command input loop in Main

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // This try-resource block automatically closes the scanner when the program exits.
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.print("> ");
+                CommandHandler.execute(scanner.nextLine());
+            }
+        }
     }
 
 }
